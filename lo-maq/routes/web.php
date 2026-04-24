116c334a74f94464823df7731cb99ddb28bdd727
WereHound <murakami_gabriel@yahoo.com>
2026-04-24 10:08:34 -0300

    AB#14 Criar Routes Equipamentos

--- lo-maq/routes/web.php
+++ lo-maq/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipamentoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/equipamentos', EquipamentoController::class);
