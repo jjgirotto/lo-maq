185877b877eb57fa79fa01db89e13c4bf0778c33
WereHound <murakami_gabriel@yahoo.com>
2026-04-24 11:39:05 -0300

    AB#36 Edicao do Migrate Usuarios para o User

--- lo-maq/database/migrations/0001_01_01_000000_create_users_table.php
+++ lo-maq/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,12 +12,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('access', ['ADM', 'CLI'])->default('CLI');
+
+            $table->string('telefone')->nullable();
+            $table->string('endereco')->nullable();
+            $table->string('cpf')->nullable();
+            $table->string('cnpj')->nullable();
+            $table->boolean('documentos_validados')->default(false);
+            $table->float('reputacao_media')->default(0);
+
             $table->rememberToken();
-            $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
